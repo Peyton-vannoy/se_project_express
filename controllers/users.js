44b013077af8add5ba427f2cb2e166672741b842
Peyton Vannoy <peyton.vannoy1999@gmail.com>
2024-08-01 15:41:30 -0400

    added login jwt token/ middleware

--- controllers/users.js
+++ controllers/users.js
@@ -1,13 +1,17 @@
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 const { ERROR_CODES, ERROR_MESSAGES } = require("../utils/errors");
+const { JWT_SECRET, JWT_EXPIRES_IN } = require("../utils/config");
 
 // GET /users
 const getUsers = (req, res) => {
   User.find({})
     .then((users) => res.status(200).send({ data: users }))
-    .catch(() => res
+    .catch(() =>
+      res
         .status(ERROR_CODES.INTERNAL_SERVER_ERROR)
-        .send({ message: ERROR_MESSAGES.INTERNAL_SERVER_ERROR }));
+        .send({ message: ERROR_MESSAGES.INTERNAL_SERVER_ERROR })
+    );
 };
 
 // GET /usersId
@@ -37,7 +41,19 @@ const getUser = (req, res) => {
 
 // POST /createUser
 const createUser = (req, res) => {
-  const { name, avatar } = req.body;
+  const { name, avatar, email, password } = req.body;
+
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error("Incorrect email or password"));
+      }
+    })
+    .catch((err) => {
+      res.status(ERROR_CODES.UNAUTHORIZED_ERROR_CODE).send({
+        message: ERROR_MESSAGES.UNAUTHORIZED_ERROR,
+      });
+    });
 
   User.create({ name, avatar })
     .then((user) => res.status(201).send({ data: user }))
@@ -54,4 +70,23 @@ const createUser = (req, res) => {
     });
 };
 
-module.exports = { getUsers, getUser, createUser };
+//Login Controller
+const login = (req, res) => {
+  const { email, password } = req.body;
+
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
+        expiresIn: JWT_EXPIRES_IN,
+      });
+      res.send({ token });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(ERROR_CODES.UNAUTHORIZED_ERROR_CODE).send({
+        message: ERROR_MESSAGES.UNAUTHORIZED_ERROR,
+      });
+    });
+};
+
+module.exports = { getUsers, getUser, createUser, login };
--- utils/errors.js
+++ utils/errors.js
@@ -1,6 +1,7 @@
 const ERROR_CODES = {
   BAD_REQUEST: 400,
   NOT_FOUND: 404,
+  UNAUTHORIZED_ERROR_CODE: 401,
   INTERNAL_SERVER_ERROR: 500,
 };
 
@@ -8,6 +9,7 @@ const ERROR_MESSAGES = {
   BAD_REQUEST: "Invalid data.",
   NOT_FOUND: "Data not found.",
   INTERNAL_SERVER_ERROR: "An error has occurred on the server.",
+  UNAUTHORIZED_ERROR: "You are not authorized.",
 };
 
 module.exports = { ERROR_CODES, ERROR_MESSAGES };
