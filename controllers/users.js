96e7f732cda701bed4acafaefcdf7f37b6f0e6ba
Peyton Vannoy <peyton.vannoy1999@gmail.com>
2024-08-02 15:53:16 -0400

    updated createUser Controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,13 +1,16 @@
 const User = require("../models/user");
 const { ERROR_CODES, ERROR_MESSAGES } = require("../utils/errors");
+const bcrypt = require("bcrypt");
 
 // GET /users
 const getUsers = (req, res) => {
   User.find({})
     .then((users) => res.status(200).send({ data: users }))
-    .catch(() => res
+    .catch(() =>
+      res
         .status(ERROR_CODES.INTERNAL_SERVER_ERROR)
-        .send({ message: ERROR_MESSAGES.INTERNAL_SERVER_ERROR }));
+        .send({ message: ERROR_MESSAGES.INTERNAL_SERVER_ERROR })
+    );
 };
 
 // GET /usersId
@@ -37,20 +40,50 @@ const getUser = (req, res) => {
 
 // POST /createUser
 const createUser = (req, res) => {
-  const { name, avatar } = req.body;
+  const { name, avatar, email, password } = req.body;
 
-  User.create({ name, avatar })
-    .then((user) => res.status(201).send({ data: user }))
+  if (!email) {
+    return res.status(ERROR_CODES.BAD_REQUEST).send({
+      message: "An email address is required",
+    });
+  }
+
+  return User.findOne({ email })
+    .then((existingEmail) => {
+      if (existingEmail) {
+        const error = new Error(ERROR_MESSAGES.EMAIL_ALREADY_EXISTS);
+        error.code = 11000;
+        throw error;
+      }
+      return bcrypt.hash(password, 10);
+    })
+    .then((hash) =>
+      User.create({
+        name,
+        avatar,
+        email,
+        password: hash,
+      })
+    )
+    .then((user) =>
+      res
+        .status(201)
+        .send({ name: user.name, email: user.email, avatar: user.avatar })
+    )
     .catch((err) => {
-      if (err.name === "ValidationError") {
-        res.status(ERROR_CODES.BAD_REQUEST).send({
-          message: ERROR_MESSAGES.BAD_REQUEST,
-        });
-      } else {
-        res.status(ERROR_CODES.INTERNAL_SERVER_ERROR).send({
-          message: ERROR_MESSAGES.INTERNAL_SERVER_ERROR,
+      if (err.code === 11000) {
+        return res.status(ERROR_CODES.CONFLICT).send({
+          message: ERROR_MESSAGES.CONFLICT,
         });
       }
+      if (err.name === "ValidationError") {
+        return res.status(ERROR_CODES.BAD_REQUEST).send({
+          message: ERROR_MESSAGES.BAD_REQUEST,
+        });
+      }
+      return res.status(ERROR_CODES.INTERNAL_SERVER_ERROR).send({
+        message: ERROR_MESSAGES.INTERNAL_SERVER_ERROR,
+      });
     });
 };
 
--- utils/errors.js
+++ utils/errors.js
@@ -2,12 +2,15 @@ const ERROR_CODES = {
   BAD_REQUEST: 400,
   NOT_FOUND: 404,
   INTERNAL_SERVER_ERROR: 500,
+  CONFLICT: 409,
 };
 
 const ERROR_MESSAGES = {
   BAD_REQUEST: "Invalid data.",
   NOT_FOUND: "Data not found.",
   INTERNAL_SERVER_ERROR: "An error has occurred on the server.",
+  EMAIL_ALREADY_EXISTS: "This email is already registered.",
+  CONFLICT: "Data already exists.",
 };
 
 module.exports = { ERROR_CODES, ERROR_MESSAGES };
