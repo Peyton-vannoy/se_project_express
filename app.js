8e969bf4a141fc279f1ed0638807a8f955fddd2f
Peyton Vannoy <peyton.vannoy1999@gmail.com>
2024-09-11 02:07:40 -0400

    refactor

--- app.js
+++ app.js
@@ -5,14 +5,13 @@ const indexRouter = require("./routes/index");
 
 const app = express();
 const { PORT = 3001 } = process.env;
+app.use(express.json());
+app.use(cors());
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
   .then(() => {})
   .catch();
-
-app.use(express.json());
-app.use(cors());
 
 app.use("/", indexRouter);
 
