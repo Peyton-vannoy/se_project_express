b3233031a09e1762c6a26c63e9af8a7c45b097fe
Peyton Vannoy <peyton.vannoy1999@gmail.com>
2024-10-12 00:21:32 -0400

    Options method

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: ["https://wtwr.ohbah.com", "http://localhost:3000"],
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
