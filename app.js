5520781b139a88aff5655c472441b7940544e499
Peyton Vannoy <peyton.vannoy1999@gmail.com>
2024-10-11 23:12:45 -0400

    hotfix for cors

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ app.use(express.json());
 app.use(
   cors({
     origin: ["https://wtwr.ohbah.com", "http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
